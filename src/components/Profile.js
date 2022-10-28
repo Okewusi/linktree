c82b312b92d2ee92ff8832d25db93b67abd949fd
pelcool <okewusioluwapelumi@gmail.com>
2022-10-28 17:36:30 -0500

    icons added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import profilePicture from "../assets/profile.JPG"
+import share from "../assets/share.jpg"
+import mobile from "../assets/mobile.jpg"
 import "./Profile.css"
 
 const Profile = () => {
   return (
     <div id='profile'>
         <div id='profileContainer'>
-            <img src={profilePicture} id='profile_img' alt='profile'/>
+          <img src={profilePicture} id='profile_img' alt='profile'/>
+          <img src={mobile} id='mobile_share' alt='profile'/>
+          <img src={share} id='share_link' alt='share button'/>
         </div>
         <p id='twitter'>Pelcool</p>
         <p id='slack'>Comfort Oluwapelumi</p>
