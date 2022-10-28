186c3d574474be28e6cd9edaa6e7ea186ec59aee
pelcool <okewusioluwapelumi@gmail.com>
2022-10-28 15:23:51 -0500

    slack icon added

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import github from "../assets/github.jpg"
+import slack from "../assets/slack.jpg"
 import "./tech.css"
 
 const Tech = () => {
   return (
     <div id='techContainer'>
-        <a href='https://github.com/Okewusi'>
-            <img src={github} alt="github" className='links' target="_blank" rel='noopener noreferrer'/>
+        <a href='https://app.slack.com/client/T042F7V19Q8/D048G3CPG2W/rimeto_profile/U0488054LMC' target="_blank" rel='noopener noreferrer'>
+          <img src={slack} alt="github" className='links'/>
+        </a>
+        <a href='https://github.com/Okewusi' target="_blank" rel='noopener noreferrer'>
+          <img src={github} alt="slack" className='links'/>
         </a>
     </div>
   )
