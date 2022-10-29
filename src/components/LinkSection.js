803ee9c8862e647b28227f6b8fa8e23b8d5859ea
pelcool <okewusioluwapelumi@gmail.com>
2022-10-29 08:12:21 -0500

    id fixed

--- src/components/LinkSection.js
+++ src/components/LinkSection.js
@@ -7,12 +7,12 @@ const LinkSection = () => {
 
   return (
     <div id='LinkSectionContainer'>
-      <Link href='https://twitter.com/pelcool' id="twitter" socials="Twitter Link" />
-      <Link href='https://training.zuri.team/' id="btn__zuri" socials="Zuri Team" />
-      <Link href='http://books.zuri.team' id="books" socials="Zuri Books" />
-      <Link href='http://books.zuri.team/python-for-begineers?ref_id=ComfortOluwapelumi' id="book_python" socials="Python Books" />
-      <Link href='https://background.zuri.team' id="pitch" socials="Background Check for Coders" />
-      <Link href='https://books.zuri.team/design-rules' id="book_design" socials="Design Books" />
+      <Link href='https://twitter.com/pelcool' name="twitter" socials="Twitter Link" />
+      <Link href='https://training.zuri.team/' name="btn__zuri" socials="Zuri Team" />
+      <Link href='http://books.zuri.team' name="books" socials="Zuri Books" />
+      <Link href='http://books.zuri.team/python-for-begineers?ref_id=ComfortOluwapelumi' name="book_python" socials="Python Books" />
+      <Link href='https://background.zuri.team' name="pitch" socials="Background Check for Coders" />
+      <Link href='https://books.zuri.team/design-rules' name="book_design" socials="Design Books" />
       <Tech />
     </div>
   )
